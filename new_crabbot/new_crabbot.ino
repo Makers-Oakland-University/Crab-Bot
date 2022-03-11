f45422a075d8a8367fc73dce2765645215ca0520
Matthew Bellafaire <matthew.bellafaire@gmail.com>
2022-03-11 18:58:33 -0500

    semi-tested: kinematic equations added for crab-bot motion

--- new_crabbot/new_crabbot.ino
+++ new_crabbot/new_crabbot.ino
@@ -11,8 +11,8 @@
 */
 
 //motor driver connections
-#define FL_DIR 13
-#define FL_PWM 14
+#define FL_DIR 14
+#define FL_PWM 13
 #define BL_DIR 18
 #define BL_PWM 17
 #define FR_DIR 25
@@ -26,12 +26,21 @@
 #define FR_CHANNEL 2
 #define BR_CHANNEL 3
 
+//drive constants for inverse kinematics
+#define WHEEL_RADIUS 0.05
+#define BOT_WIDTH 0.410
+#define BOT_LENGTH 0.3
+
+#define A_CONST (1 / WHEEL_RADIUS)
+#define B_CONST (A_CONST)
+#define C_CONST (0.5 * ((BOT_WIDTH + BOT_LENGTH) / WHEEL_RADIUS))
+
 
 void setup()
 {
   Serial.begin(115200);
   initMotors();
-  motor_test();
+//  motor_test();
 
   initWebControl();
 }
@@ -49,6 +58,13 @@ void loop()
 
 void onNewUserInput() {
   Serial.printf("Control Values x:%d y:%d yaw:%d \n", x_input, y_input, yaw_input);
+  //  inverseKinematics(x_input, y_input, yaw_input);
+
+  double x = x_input / 100.0;
+  double y = y_input / 100.0;
+  double yaw = yaw_input / 100.0;
+
+  inverseKinematics(x, y, yaw);
 }
 
 
@@ -85,41 +101,97 @@ void initMotors() {
 }
 
 void motor_test() {
+  double step_delay = 50.0;
   Serial.println("Testing Front Left Motor");
   for (float a = -1.0; a < 1.0; a += 0.1) {
     driveMotors(a, 0, 0, 0);
-    delay(100);
+    delay(step_delay);
   }
 
   Serial.println("Testing Back Left Motor");
   for (float a = -1.0; a < 1.0; a += 0.1) {
     driveMotors(0, a, 0, 0);
-    delay(100);
+    delay(step_delay);
   }
 
 
   Serial.println("Testing Back Right Motor");
   for (float a = -1.0; a < 1.0; a += 0.1) {
     driveMotors(0, 0, a, 0);
-    delay(100);
+    delay(step_delay);
   }
 
   Serial.println("Testing Front Right Motor");
   for (float a = -1.0; a < 1.0; a += 0.1) {
     driveMotors(0, 0, 0, a);
-    delay(100);
+    delay(step_delay);
   }
 
   driveMotors(0, 0, 0, 0);
+
+  double kinematic_delay = 800;
+
+  Serial.println("Moving Right");
+  inverseKinematics(1.0, 0, 0);
+  delay(kinematic_delay);
+  Serial.println("Moving Left");
+  inverseKinematics(-1.0, 0, 0);
+  delay(kinematic_delay);
+
+  Serial.println("Moving Forward");
+  inverseKinematics(0, 1.0, 0);
+  delay(kinematic_delay);
+
+  Serial.println("Moving Backward");
+  inverseKinematics(0, -1.0, 0);
+  delay(kinematic_delay);
+
+
+  Serial.println("Rotating clockwise");
+  inverseKinematics(0, 0, 1.0);
+  delay(kinematic_delay);
+
+  Serial.println("Rotating conterclockwise");
+  inverseKinematics(0, 0, -1.0);
+  delay(kinematic_delay);
+
+  inverseKinematics(0, 0, 0);
 }
+
+//convert x,y, and angular velocity to the values needed in order to move crab bot
+void inverseKinematics(double x, double y, double yaw) {
+
+  //if you're wondering why these are reversed, I have no idea.
+  double x_vel = y / 20.0 ;
+  double y_vel = x / 20.0;
+  double psi = -(yaw / 20.0);
+
+  //  double w1 =  A_CONST * x_vel - B_CONST * y_vel - C_CONST * psi;
+  //  double w3 = A_CONST * x_vel + B_CONST * y_vel + C_CONST * psi;
+  //  double w4 = A_CONST * x_vel + B_CONST * y_vel - C_CONST * psi;
+  //  double w2 =  A_CONST * x_vel - B_CONST * y_vel + C_CONST * psi;
+
+  double w1 = A_CONST * x_vel - B_CONST * y_vel - C_CONST * psi;
+  double w2 = A_CONST * x_vel + B_CONST * y_vel - C_CONST * psi;
+  double w3 = A_CONST * x_vel - B_CONST * y_vel + C_CONST * psi;
+  double w4 = A_CONST * x_vel + B_CONST * y_vel + C_CONST * psi;
+
+  Serial.printf("Driving motors (%f, %f, %f, %f)\n", w1, w2, w3, w4);
+
+  driveMotors( w1, w2, w3, w4  );
+}
+
+
 
 //sets motor values between -1 and 1 for front left, back left, back right, and front right respectively
 void driveMotors(float fl, float bl, float br, float fr) {
 
+  Serial.printf("Driving motors (%f, %f, %f, %f)\n", fl, bl, br, fr);
+
+
   //set direction and PWM for each motor
   //set PWM based on value from -1 to 1
   ledcWrite(FL_CHANNEL, (int)abs(255.0 * fl));
-
   //if input is greater than 0 then write high, otherwise write low.
   digitalWrite(FL_DIR, fl > 0);
 
@@ -129,7 +201,7 @@ void driveMotors(float fl, float bl, float br, float fr) {
   digitalWrite(BL_DIR, bl > 0);
 
   ledcWrite(BR_CHANNEL, (int)abs(255.0 * br));
-  digitalWrite(BR_DIR, br < 0); //the right side motors are inverted 
+  digitalWrite(BR_DIR, br < 0); //the right side motors are inverted
 
   ledcWrite(FR_CHANNEL, (int)abs(255.0 * fr));
   digitalWrite(FR_DIR, fr < 0);
